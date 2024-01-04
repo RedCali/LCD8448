6ae6db44271d6866af54e4c5587d67237c507e42
BastianGeier <mail@bastiangeier.de>
2024-01-04 13:27:11 +0100

    add Arduino Example

---
+++ examples/LCD8448_simple/LCD8448_Config.h
@@ -0,0 +1,47 @@
+/*
+* LCD Display Library for the 8448 LCD Display (Nokia 5110 display)
+* Base		: Lauren from DFRobot / version 0.3
+* Date     : 06.01.2012
+* E-mail   : Lauran.pan@gmail.com
+
+* modified	: Dominik and Bastian
+* E-mail	: mail@bastiangeier.de
+
+* Any suggestions are welcome.
+*/
+#pragma once
+#ifndef LCD8448_CONFIG_H_
+#define LCD8448_CONFIG_H_
+
+// Commend / uncommend the define for Debugging
+//#define LCD_DEBUG
+
+// Define here if backlight signal is inverted
+#define BACKLIGHT_INVERTED
+
+// LCD PIN definition
+#if defined(ARDUINO) && ARDUINO >= 100  // SPI Interface --- (using Arduino Digital Pin 2,3,4,5,6)
+#define LCD_BL 7                        // Back light control
+#define SPI_SCK 6                       // CLK - Serial Clock(Master Output)
+#define SPI_MOSI 5                      // DI - Master Output,Slave Input
+#define LCD_DC 4                        // DC - Data/Command(command active low)
+#define SPI_CS 3                        // CE - Chip Select,Slave Transmit Enable(active low,Master Output)
+#define LCD_RST 2                       // Reset
+
+#else
+#define LCD_PORT PORTB
+#define LCD_DDR DDRB
+#define LCD_RST PINB0   // RST	Reset
+#define SPI_CS PINB1    // CE	Chip Select,Slave Transmit Enable(active low,Master Output)
+#define LCD_DC PINB2    // DC	Data/Command	(command active low)
+#define SPI_MOSI PINB3  // Din	Master Output,	Slave Input
+// #define	SPI_MISO	PINB4		// nc	Master Input,	Slave Output
+#define SPI_SCK PINB5   // CLK	Serial Clock(Master Output)
+
+// Back light definition
+#define LCD_BL_PORT PORTD
+#define LCD_BL_DDR DDRD
+#define LCD_BL PIND7  // Back light control
+#endif
+
+#endif /* LCD8448_CONFIG_H_ */
---
+++ examples/LCD8448_simple/LCD8448_simple.ino
@@ -0,0 +1,71 @@
+// Include LCD definitions - Overwrite the Library LCD Config File
+#include "LCD8448_Config.h"
+// Include LCD Library
+#include <LCD8448.h>
+
+// Define the update rate of the display and the LED
+#define UPDATE_INTERVAL 500
+
+// LED Definitions
+#define LED_PIN 13
+unsigned char _pinState = 0x00;
+
+// Diaplay Variables
+// Text Backfround definition
+LCD8448::LCD_Display _displayHighlight = LCD8448::NORMAL;
+// Nummeric counter
+unsigned long _counter = 0;
+// Char array for numericc convertion
+char _bufferCounter[15];
+
+// Storage for Runtime measuring for interval control
+unsigned long _millisPrevious;
+
+void setup() {
+  // Initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_PIN, OUTPUT);
+  // Init LCD Display
+  initLCD();
+
+  // Write constant strings into the display
+  lcd.write_string(1 * 6 + 3, 0, "Hello", LCD8448::NORMAL);
+  lcd.write_string(8 * 6 - 3, 0, "World", LCD8448::INVERTED);
+// Switch on the Backlight of the LCD
+  lcd.setBacklightON();
+}
+
+void loop() {
+  // Everytime the runtime exeeds the update intervll, we update the display
+  if ((millis() - _millisPrevious) > UPDATE_INTERVAL) {
+    // Write LED Pin
+    digitalWrite(LED_PIN, _pinState);
+    // Toggle LED Pin state
+    _pinState = (_pinState == 0) ? 0x01 : 0x00;
+
+    // Convert numeric value into string using "sprintf", incremnt the counter afterwarts and write to display
+    sprintf(_bufferCounter, "%3d.%d", (int)(abs(_counter) / 10), (int)(abs(_counter) % 10));
+    _counter++;
+
+    lcd.write_string(24, 4, _bufferCounter, LCD8448::NORMAL);
+
+    // Just a simple string where the background highlighting toggles
+    lcd.write_string(1 * 6 + 3, 2, "The LCD8448", _displayHighlight);
+    _displayHighlight = (_displayHighlight == LCD8448::NORMAL) ? LCD8448::INVERTED : LCD8448::NORMAL;
+
+    // Store actull time counter
+    _millisPrevious = millis();
+    // Reset the counter if reaches 1000
+    if (_counter >= 1000) _counter = 0;
+  }
+}
+
+
+void initLCD() {
+  Serial.println("Init Display....");
+  lcd.init();
+}
+
+void stopLCD() {
+  Serial.println("Stop Display....");
+  lcd.off();
+}
